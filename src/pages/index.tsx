c82c3be0792704f62d306149a00cf6ead68101d6
John Grisham <john.grisham@gatewayapps.com>
2021-02-01 15:58:46 -0600

    Updated SEO title.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,7 +4,7 @@ import { Landing, Layout, SEO } from '../components'
 const HomePage = () => {
 	return (
 		<Layout>
-			<SEO title="They go together" previewImage={{ width: 267 }} />
+			<SEO title="Gatsby Template" previewImage={{ width: 267 }} />
 			<Landing
 				heading="Get the best of both worlds"
 				subHeading="Fast and flexible template"
